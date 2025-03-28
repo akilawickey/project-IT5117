d4735388c347ea2bf5a28c4232801a8558ef05ad
Lahiru Lakshitha Samaranayaka <lahiru.lls@gmail.com>
2025-03-28 07:13:38 -0500

    Hotels are now included in the Google Maps route
    Hotels are now included in the Google Maps route
    
    After computing the itinerary, the result is now stored in self.last_route
    
    This allows the “Open in Google Maps” button to work properly

--- app/map_visualizer.py
+++ app/map_visualizer.py
@@ -1,20 +1,27 @@
 
 import folium
+import os
+import webbrowser
 from app.settings import DEFAULT_MAP_CENTER, MAP_FILE
 from app.utils import get_latlng
 
 def generate_map(start, end, waypoints, hotel_names=None):
     m = folium.Map(location=DEFAULT_MAP_CENTER, zoom_start=7)
-    for idx, location in enumerate(waypoints):
-        color = 'blue' if hotel_names and location in hotel_names else 'red'
-        folium.Marker(location=get_latlng(location), popup=location, icon=folium.Icon(color=color)).add_to(m)
+    coordinates = []
+    for place in waypoints:
+        latlng = get_latlng(place)
+        coordinates.append(latlng)
+        is_hotel = hotel_names and place in hotel_names
+        marker_color = 'blue' if is_hotel else 'red'
+        folium.Marker(
+            location=latlng,
+            popup=place,
+            icon=folium.Icon(color=marker_color)
+        ).add_to(m)
+    if len(coordinates) > 1:
+        folium.PolyLine(locations=coordinates, color="green", weight=3).add_to(m)
     m.save(MAP_FILE)
 
-
-import webbrowser
-import os
-
 def open_map():
-    from app.settings import MAP_FILE
     map_path = os.path.abspath(MAP_FILE)
     webbrowser.open(f"file://{map_path}")
